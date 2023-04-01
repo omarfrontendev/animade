5cb9578589b572dc0fd50faa5738e1ad2e60d6dd
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2023-04-01 22:33:40 +0200

    updates

--- src/pages/ChooseProdutcs/index.jsx
+++ src/pages/ChooseProdutcs/index.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Button, FooterBtns } from "../../components";
 import ProductCard from "../../components/ProductCard";
 import { IoIosAdd } from "react-icons/io";
-import styles from "./.module.scss";
 import { Link } from "react-router-dom";
+import styles from "../ChoosePlatform/.module.scss";
+import productStyles from "./.module.scss";
 
 const ChooseProdutcs = () => {
   return (
     <>
       <div className={styles.page}>
-        <h3 className="section__title">
+        <h3 className="section__title page__title">
           Which products would you like to print on?
         </h3>
-        <div className={styles.products__section}>
-          <div className={styles.products__header}>
-            <h5 className={styles.products__title}>
+        <div
+          className={`${productStyles.products__section} ${styles.platform__section}`}
+        >
+          <div className={styles.platform__header}>
+            <h5 className={styles.platform__title}>
               Choose from your Printful Templates
             </h5>
             <Link to="/" className={styles.add__new}>
@@ -22,7 +25,7 @@ const ChooseProdutcs = () => {
               New Template
             </Link>
           </div>
-          <div className={styles.products__list}>
+          <div className={styles.platform__list}>
             {new Array(3).fill("").map((_, i) => (
               <ProductCard key={i} />
             ))}
