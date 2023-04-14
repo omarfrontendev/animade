2c53c48112dd00de984f0a327475af30427a5c87
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2023-04-14 20:56:47 +0200

    updates

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+const BASE__URL = process.env.BASE__URL;
+
+// const
+
+// export
--- src/pages/SingleInput/index.jsx
+++ src/pages/SingleInput/index.jsx
@@ -8,29 +8,30 @@ const SingleInput = () => {
 
   const generateImages = async () => {
     try {
-      const res = await fetch(
+      const jsone = JSON.stringify({
+        key: "kj36kb5bvU5gR8liQoTHe4T1IBHentRZYqqwuVb0cEozZPb3fiUM51jjVaCp",
+        prompt:
+          "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
+        negative_prompt:
+          "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime",
+        width: "512",
+        height: "512",
+        samples: "1",
+        num_inference_steps: "20",
+        seed: null,
+        guidance_scale: 7.5,
+        webhook: null,
+        track_id: null,
+      });
+
+      const res = await axios.post(
         "https://stablediffusionapi.com/api/v3/text2img",
+        jsone,
         {
-          method: "POST",
-          body: JSON.stringify({
-            key: "kj36kb5bvU5gR8liQoTHe4T1IBHentRZYqqwuVb0cEozZPb3fiUM51jjVaCp",
-            prompt:
-              "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
-            negative_prompt:
-              "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime",
-            width: "512",
-            height: "512",
-            samples: "1",
-            num_inference_steps: "20",
-            seed: null,
-            guidance_scale: 7.5,
-            webhook: null,
-            track_id: null,
-          }),
           headers: {
+            // Overwrite Axios's automatically set Content-Type
             "Content-Type": "application/json",
           },
-          mode: "no-cors",
         }
       );
       const responseData = await res.json();
